edcadb1786f594fbea7e5289124903ce8f4b01ba
Simon Oulevay <simon.oulevay@gmail.com>
2015-08-26 19:55:26 +0200

    Generated from source @782580b.

--- README.md
+++ README.md
@@ -1,23 +1,80 @@
 # Probe Dock Site
 
-Jekyll site for Probe Dock.
+Jekyll site for [Probe Dock](https://github.com/probedock/probedock).
 
+The `source` branch contains the source of the Jekyll blog.
+That's where you should work to update the blog.
+
+The `master` branch contains the compiled version that is served by GitHub pages.
+
+
+
+* [Requirements](#requirements)
+* [First-time setup](#setup)
+* [Local development](#development)
+* [Staging deployment (Amazon S3)](#staging)
+* [Production deployment (GitHub Pages)](#production)
+
+
+
+<a name="requirements"></a>
 ## Requirements
 
 * Ruby 2.x
 
+
+
+<a name="setup"></a>
 ## First-time setup
 
 * `git clone git@github.com:probedock/probedock.github.io.git`
 
 * `cd probedock.github.io.git`
 
+* `git checkout source`
+
 * `bundle install`
 
-## Serve locally
+The following step is required to push the blog to Amazon S3 for staging:
 
-* `jekyll serve`
+* Create a `.env` file in the root directory of the repository, with the following contents:
 
-## Build from current directory and push to GitHub Pages
+  ```
+  S3_KEY=your-amazon-access-key-id
+  S3_SECRET=your-amazon-access-key-secret
+  ```
+
+
+
+<a name="development"></a>
+## Local development
+
+Run the following task to serve the site locally and open it in your browser:
+
+* `bundle exec rake preview`
+
+
+
+<a name="staging"></a>
+## Staging deployment
+
+Run the following task to build the site and upload it to Amazon S3 for preview:
+
+* `bundle exec rake staging`
+
+Go to [http://probedock-blog-staging.s3-website.eu-central-1.amazonaws.com/](http://probedock-blog-staging.s3-website.eu-central-1.amazonaws.com/) to view the generated site.
+
+**Note:** this builds the site from the current directory with drafts enabled.
+
+
+
+<a name="production"></a>
+## Production deployment
+
+Run the following task to build the site and push it to GitHub Pages:
 
 * `bundle exec rake pages`
+
+Go to [http://probedock.io/](http://probedock.io/) to view the generated site.
+
+**Note:** this builds from the latest commit on the *local master branch* (local modifications will *not* be included) with drafts disabled.
--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,76 +1,23 @@
 $(document).ready(function() {
-    
-    /* ======= Blog Featured Post Slideshow - Flexslider ======= */ 
-    $('.blog-slider').flexslider({
-        animation: "fade",
-        slideshowSpeed: 8000
-    });
-    
-   
-    /* ======= Blog page masonry ======= */
-    /* Ref: http://desandro.github.io/masonry/index.html */
-    
-    var $container = $('#blog-mansonry');
-    $container.imagesLoaded(function(){
-        $container.masonry({
-            itemSelector : '.post'
-        });
-    });
-    
-   /* ======= Blog page searchbox ======= */
-   /* Ref: http://thecodeblock.com/expanding-search-bar-with-jquery-tutroial/ */
-    var submitIcon = $('.searchbox-icon');
-    var inputBox = $('.searchbox-input');
-    var searchBox = $('.searchbox');
-    var isOpen = false;
-    submitIcon.on('click', function(){
-        if(isOpen === false){
-            searchBox.addClass('searchbox-open');
-            inputBox.focus();
-            isOpen = true;
-        } else {
-            searchBox.removeClass('searchbox-open');
-            inputBox.focusout();
-            isOpen = false;
-        }
-    }); 
-    
-    submitIcon.mouseup(function(){
-        return false;
-    });
-    searchBox.mouseup(function(){
-        return false;
-    });
-    $(document).mouseup(function(){
-        if(isOpen === true){
-            $('.searchbox-icon').css('display','block');
-            submitIcon.click();
-        }
-    });
-    
-    function buttonUp(){
-        var inputVal = $('.searchbox-input').val();
-        inputVal = $.trim(inputVal).length;
-        if( inputVal !== 0){
-            $('.searchbox-icon').css('display','none');
-        } else {
-            $('.searchbox-input').val('');
-            $('.searchbox-icon').css('display','block');
-        }
+
+  $.extend($.scrollTo.defaults, {
+    axis: 'y'
+  });
+
+  console.log('foo');
+  $('.blog-entry-content a').on('click', function(event) {
+
+    var target = $(event.target);
+    if (target.is('a') && target.attr('href').match(/^#/)) {
+      event.preventDefault();
+
+      var targetName = target.attr('href').replace(/^#/, '');
+
+      $.scrollTo($('[name=' + targetName + ']'), 650, {
+        offset: -90
+      });
+
+      window.location.hash = targetName;
     }
-    
-    inputBox.keyup(function() {
-        buttonUp();
-    });
-    
-    //Make sure the "Go" button is not shown when resize the browser window from mobile to desktop
-    $(window).resize(function(){
-        $('.searchbox-icon').css('display','block');
-        searchBox.removeClass('searchbox-open');        
-    });
-
-    
+  });
 });
-
-
-
